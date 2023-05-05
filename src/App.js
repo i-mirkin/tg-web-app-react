9edc1366ac29ad5bfa9cf7e07609d50dda7c0537
i-mirkin <i.mirkin.mail@gmail.com>
2023-05-05 21:52:04 +0300

    initial

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import './App.css';
+import {useEffect} from "react";
+
+const tg = window.Telegram.WebApp
+
+useEffect ( () => {
+    tg.ready();
+}, [])
+const onClose = () =>{
+   tg.close()
+}
+
+
+function App() {
+  return (
+    <div className="App">
+
+        <button onClick={onClose}>Закрыть</button>
+
+    </div>
+  );
+}
+
+export default App;
