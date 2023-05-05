4b3a7da4789ddf082ca67f75e8a081940298a203
i-mirkin <i.mirkin.mail@gmail.com>
2023-05-05 22:34:18 +0300

    init

--- src/index.js
+++ src/index.js
@@ -2,8 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
-//window.Telegram.WebApp.BackButton
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
